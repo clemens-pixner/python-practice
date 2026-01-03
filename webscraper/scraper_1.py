7d45348ff6f1a805152aaf5b9ab007a30b9ebb95
clemens-pixner <clemenspixner2.0@gmail.com>
2026-01-03 19:22:04 +0100

    Refactor web scraper to paginate through book listings

--- webscraper/scraper_1.py
+++ webscraper/scraper_1.py
@@ -1,15 +1,25 @@
 import requests 
 from bs4 import BeautifulSoup
 
-url = "https://books.toscrape.com"
-response = requests.get(url)
-response.encoding = "utf-8"
-html = response.text
-soup = BeautifulSoup(html, "html.parser")
+page = 1
 
-for article in soup.find_all("article", class_="product_pod"):
-    book_name = article.h3.a["title"]
-    book_price = article.find("p", class_= "price_color").get_text(strip=True)
-    print(book_name)
-    print(book_price)   
-    print()
+while True:
+    url = f"https://books.toscrape.com/catalogue/page-{page}.html"
+    response = requests.get(url)
+    if response.status_code != 200:
+        break
+
+    
+    response.encoding = "utf-8"
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    for article in soup.find_all("article", class_="product_pod"):
+        book_name = article.h3.a["title"]
+        book_price = article.find("p", class_= "price_color").get_text(strip=True)
+        availability = article.find("p", class_= "instock availability").get_text(strip=True)
+        print(book_name)
+        print(book_price)
+        print(availability)  
+        print()
+    
+    page += 1
